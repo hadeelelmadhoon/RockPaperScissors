28cda093e633b598babe12cbd4a8f874a43b436a
hadeelelmadhoon <44147485+hadeelelmadhoon@users.noreply.github.com>
2018-11-17 15:21:59 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,11 +1,3 @@
-//
-//  Player.cpp
-//  assn2
-//
-//  Created by Hadeel Elmadhoon on 2018-10-24.
-//  Copyright © 2018 Hadeel Elmadhoon. All rights reserved.
-//
-
 #include "Player.h"
 #include <iostream>
 using namespace std;
