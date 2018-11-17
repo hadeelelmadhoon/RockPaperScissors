fc73acbe5184a43071df1504cf55f6c38af554ed
hadeelelmadhoon <44147485+hadeelelmadhoon@users.noreply.github.com>
2018-11-17 15:22:18 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,11 +1,3 @@
-//
-//  Player.hpp
-//  assn2
-//
-//  Created by Hadeel Elmadhoon on 2018-10-24.
-//  Copyright © 2018 Hadeel Elmadhoon. All rights reserved.
-//
-
 #pragma once
 #ifndef Player_h
 #define Player_h
