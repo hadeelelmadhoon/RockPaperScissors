69d6a01ddfc90650276559dc30ad199e4427e403
hadeelelmadhoon <44147485+hadeelelmadhoon@users.noreply.github.com>
2018-11-17 15:18:07 -0500

    Update Computer.cpp

--- Computer.cpp
+++ Computer.cpp
@@ -1,11 +1,3 @@
-//
-//  Computer.cpp
-//  assn2
-//
-//  Created by Hadeel Elmadhoon on 2018-10-24.
-//  Copyright © 2018 Hadeel Elmadhoon. All rights reserved.
-//
-
 #include "Computer.h"
 #include <iostream>
 using namespace std;
